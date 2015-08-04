ce14f7ee7ad6e252df79308a64cf4a330b01baa8
marcelo <mmartche@hotmail.com>
2015-08-03 23:52:13 -0300

    layout done

--- src/app/about/_about.tpl.jsx
+++ src/app/about/_about.tpl.jsx
@@ -3,26 +3,27 @@ m('.about content-center',[
   m('hr'),
   m('h2','Comparar Planos'),
   m('form.btn-group[role="group"][onsubmit={u.silence()}]',[
-    m('.col-md-3',[
+    m('.col-md-4',[
       m('label','Origem'),
-      m('select.form-control.input-lg#select_origem',[
+      m('select.form-control#select_origem',{onchange:ctrl.calcula},[
         m('option','Escolha a cidade'),
         rows
       ])
     ]),
-    m('.col-md-3',[
+    m('.col-md-4',[
       m('label','Destino'),
-      m('select.form-control.input-lg#select_destino',[
+      m('select.form-control#select_destino',{onchange:ctrl.calcula},[
         m('option','Escolha a cidade'),
         rows
       ])
     ]),
-    m('.col-md-3',[
-      m('label','Tempo'),
-      m('input#text_tempo.input-lg[placeholder="Tempo"]'),
-    ]),
-    m('.col-md-3',[
-      <button class="input-lg" onclick={ctrl.calcula}>Calcular</button>
+    m('.col-md-4',[
+      m('label.content-center','Tempo'),
+      m('.input-group', [
+        m('input#text_tempo.form-control',{placeholder:"Tempo...",onkeyup:ctrl.calcula}),
+        <span class="input-group-addon" id="basic-addon2">minutos</span>
+      ])
+
     ])
   ]),
   m('hr'),
